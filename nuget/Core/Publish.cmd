7c0bafb91fcd69ca0b12f594d3ef4cd8afbb6fa9
jesus.lopez <jesus.lopez@i-nercya.com>
2017-05-12 13:36:06 +0200

    publish nuget commands

--- nuget/Core/Publish.cmd
+++ nuget/Core/Publish.cmd
@@ -1,3 +1,3 @@
 %~dp0..\NuGet.exe Update -self
-%~dp0..\NuGet.exe Push EntityLite.Core.1.14.1.nupkg
+%~dp0..\NuGet.exe Push EntityLite.Core.1.14.1.nupkg -source https://www.nuget.org
 pause
--- nuget/Generators/Publish.cmd
+++ nuget/Generators/Publish.cmd
@@ -1,3 +1,3 @@
 %~dp0..\NuGet.exe Update -self
-%~dp0..\NuGet.exe Push EntityLite.1.13.0.nupkg
+%~dp0..\NuGet.exe Push EntityLite.1.14.1.nupkg -source https://www.nuget.org
 pause
