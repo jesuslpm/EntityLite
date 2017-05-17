23cd8a147b4e6a7a4bcc6f49ced8cea5c0f19d21
jesus.lopez <jesus.lopez@i-nercya.com>
2017-05-17 14:03:51 +0200

    prepare for nuget publish 1.15.0

--- nuget/Core/Publish.cmd
+++ nuget/Core/Publish.cmd
@@ -1,3 +1,3 @@
 %~dp0..\NuGet.exe Update -self
-%~dp0..\NuGet.exe Push EntityLite.Core.1.14.1.nupkg -source https://www.nuget.org
+%~dp0..\NuGet.exe Push EntityLite.Core.1.15.0.nupkg -source https://www.nuget.org
 pause
--- nuget/Generators/Publish.cmd
+++ nuget/Generators/Publish.cmd
@@ -1,3 +1,3 @@
 %~dp0..\NuGet.exe Update -self
-%~dp0..\NuGet.exe Push EntityLite.1.14.1.nupkg -source https://www.nuget.org
+%~dp0..\NuGet.exe Push EntityLite.1.15.0.nupkg -source https://www.nuget.org
 pause
