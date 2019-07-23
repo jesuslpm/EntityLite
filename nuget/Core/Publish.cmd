48ccab01dbcc9a50a84be52a55414aa738604471
jesus.lopez <jesus.lopez@i-nercya.com>
2019-07-23 14:39:04 +0200

    Beta4

--- nuget/Core/Publish.cmd
+++ nuget/Core/Publish.cmd
@@ -1,3 +1,3 @@
 %~dp0..\NuGet.exe Update -self
-%~dp0..\NuGet.exe Push EntityLite.Core.2.9.0-Beta3.nupkg -source https://www.nuget.org
+%~dp0..\NuGet.exe Push EntityLite.Core.2.9.0-Beta4.nupkg -source https://www.nuget.org
 pause
