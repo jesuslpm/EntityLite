294adcfc8cbd61edd6c496853ec87495159f0137
Jesús López <jesus.lopez@inercya.com>
2022-04-17 10:57:02 +0200

    publish 2.13.0

--- nuget/Core/Publish.cmd
+++ nuget/Core/Publish.cmd
@@ -1,3 +1,3 @@
 %~dp0..\NuGet.exe Update -self
-%~dp0..\NuGet.exe Push EntityLite.Core.2.12.3.nupkg -source https://www.nuget.org
+%~dp0..\NuGet.exe Push EntityLite.Core.2.13.0.nupkg -source https://www.nuget.org
 pause
