baaf99bcbf1c6165a4fa535c90001cee3a72874f
Jesus López <jesus.lopez@inercya.com>
2024-06-16 11:29:21 +0100

    publish EntityLiteCore

--- nuget/Core/Build.cmd
+++ nuget/Core/Build.cmd
@@ -1,4 +1,4 @@
-dotnet build %~dp0..\..\inercya.EntityLite\inercya.EntityLite.Core.csproj -c Release
+dotnet build %~dp0..\..\inercya.EntityLite\inercya.EntityLite.Core.csproj -c Release  -p:ContinuousIntegrationBuild=true 
 %~dp0..\NuGet.exe Update -self
 REM %~dp0..\NuGet.exe SetApiKey MyApiKey
 %~dp0..\NuGet.exe Pack EntityLite.Core.nuspec
--- nuget/Core/Publish.cmd
+++ nuget/Core/Publish.cmd
@@ -1,3 +1,3 @@
 %~dp0..\NuGet.exe Update -self
-%~dp0..\NuGet.exe Push EntityLite.Core.2.17.0.nupkg -source https://www.nuget.org
+%~dp0..\NuGet.exe Push EntityLite.Core.3.0.0-beta1.nupkg -source https://www.nuget.org
 pause
