d7476c39a6692e15ee8454ef42c15ba667c430e7
Jesus López <jesus.lopez@inercya.com>
2024-08-23 14:07:46 +0100

    add net 8 to RevenDbProfiler

--- nuget/RavenDbProfiler/Publish.cmd
+++ nuget/RavenDbProfiler/Publish.cmd
@@ -1,3 +1,3 @@
 %~dp0..\NuGet.exe Update -self
-%~dp0..\NuGet.exe Push EntityLite.RavenDbProfiler.3.0.0-beta4.nupkg -source https://www.nuget.org
+%~dp0..\NuGet.exe Push EntityLite.RavenDbProfiler.3.0.0-beta5.nupkg -source https://www.nuget.org
 pause
