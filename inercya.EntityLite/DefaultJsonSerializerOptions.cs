eb61ef40050d61ef70850771d0fbe8d396bb482a
Jesus López <jesus.lopez@inercya.com>
2025-08-27 10:35:59 +0100

    Customize JsonNode string conversion

---
+++ inercya.EntityLite/DefaultJsonSerializerOptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace inercya.EntityLite
+{
+	internal static class DefaultJsonSerializerOptions
+	{
+		public static readonly JsonSerializerOptions Instance = new JsonSerializerOptions(JsonSerializerDefaults.Web)
+		{
+			WriteIndented = true,
+			DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+			Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+		};
+	}
+}
