143dca1f699f7cc575d434277bc58601ad8ad089
Jesus López <jesus.lopez@inercya.com>
2025-08-27 10:57:05 +0100

    Upgrage nuget packages

--- nuget/Core/Publish.cmd
+++ nuget/Core/Publish.cmd
@@ -1,3 +1,3 @@
 %~dp0..\NuGet.exe Update -self
-%~dp0..\NuGet.exe Push EntityLite.Core.3.0.0.nupkg -source https://www.nuget.org
+%~dp0..\NuGet.exe Push EntityLite.Core.3.1.0.nupkg -source https://www.nuget.org
 pause
